cf5e28f875f260fec9f928636b314669ed65873c
Anna <8pipopolam@gmail.com>
2023-03-29 16:01:15 +0300

    ffmpeg0

---
+++ converter/converter.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	//assert "github.com/stretchr/testify/assert"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"io/fs"
+
+	//"io/fs"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+var direct = "/Work/hakaton_mts/converter/" //ioutil.ReadDir("/films/")
+
+func main() {
+	fmt.Println("ща законвертим")
+	readFileInfo("mp4")
+
+}
+
+func readFileInfo(prefix string) {
+	if prefix == "" {
+		fmt.Println("some troubles")
+	}
+	//films/4th-of-july-fireworks-4k.mp4
+	files, err := ioutil.ReadDir(direct)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//оставляю для отладки
+	//for _, file := range files {
+	//	fmt.Println(file.Name(), file.IsDir())
+	//}
+	for _, file := range files {
+		if strings.Contains(file.Name(), prefix) {
+			fmt.Println(file.Name(), file.IsDir())
+			convert2gif(file.Name())
+			//prepare(file, 1)
+		}
+	}
+
+}
+
+func convert2gif(fileName string) {
+	cmd := "ffmpeg -i converter/clone.mp4 -vf \"fps=5,scale=320:-1:flags=lanczos\" -c:v pam -f image2pipe - | convert -delay 5 - -loop 0 -layers optimize test.gif"
+	_, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Failed to execute command: %s", cmd))
+	}
+}
+
+func prepare(file fs.FileInfo, typename int) {
+	if typename <= 0 {
+		fmt.Println("incorrect parameters")
+	}
+	err := ffmpeg.Input(file.Name(), ffmpeg.KwArgs{"ss": 1}).
+		Output(file.Name()+"out", ffmpeg.KwArgs{"t": 1}).OverWriteOutput().Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//assert.Nil(t, err)
+
+}
+
+//# def test_ffmpeg_interface(self):
+//#     input = Input('/old')
+//#     output = Output('/new')
+//#
+//#     ffmpeg = FFmpeg('ffmpeg', input, output)
+//#     self.assertEqual(list(ffmpeg), ['ffmpeg', '-i', '/old', '/new'])
+
+// def duration(self, file):
+//#     p = self.Popen(FFmpeg.cmds_probe+" -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 " + file)
+//#     output, _ = p.communicate()
+//#     return output.decode('ascii')[:-4]
+
+//ffprobe -show_format -show_streams test.mp4
---
+++ converter/go.mod
@@ -0,0 +1,7 @@
+module hakaton_mts
+
+go 1.20
+
+require (
+	"github.com/u2takey/ffmpeg-go" v0.4.1
+)
